0a57b1474c96d70f9e01cf4b57671c810892d1a4
Victor Ndaba <mwangindaba@gmail.com>
2023-01-09 14:27:46 +0300

    chore: setup basic routing

---
+++ src/pages/feed.tsx
@@ -0,0 +1,13 @@
+export function Feed() {
+  return (
+    <div>
+      <h1>Your feed appears here. This is a placeholder for now.</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim error
+        atque, est exercitationem corrupti eaque, veniam dolorum aut, reiciendis
+        cupiditate earum deserunt consequatur a aliquid magni quam at magnam
+        numquam.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+export function Home() {
+  return (
+    <main>
+      <h1>Random Journals</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <p className="text-3xl font-medium">
+          A simple app to anonymously share your daily thoughts and feelings
+          with people from around the world.
+        </p>
+      </div>
+    </main>
+  );
+}
