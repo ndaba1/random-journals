28b354182572a431059550b077608aece3675f6b
Victor Ndaba <mwangindaba@gmail.com>
2023-01-10 12:32:47 +0300

    refactor: updated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages";
-import { Feed } from "./pages/feed";
+import { Index } from "./pages";
+import { NotFound } from "./pages/404";
+import { CompleteSignIn } from "./pages/finish";
+import { Home } from "./pages/home";
 
 export function App() {
   return (
     <BrowserRouter>
+      <Toaster />
       <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/feed" element={<Feed />} />
+        <Route index path="/" element={<Index />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/complete" element={<CompleteSignIn />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
